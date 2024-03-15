8038278b422ea6525d4104091ae5cfe7c039d8dd
ilhmwisnu <ilhamwisnu42@gmail.com>
2024-03-15 16:22:34 +0700

    fix: fix lint

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,27 +1,26 @@
 import LinkButton from "@/components/LinkButton";
 import Image from "next/image";
 
-
 const Header = () => {
   return (
     <div className="flex flex-col lg:flex-row lg:py-24 justify-between ">
       <div className="mt-10 lg:w-3/5 text-center lg:text-left ">
         <p className="text-4xl mb-6">
-          Hi! 👋, I'm <span className="text-primary font-semibold">Wisnu</span>{" "}
-          - an IT Guy
+          Hi! 👋, I&apos;m{" "}
+          <span className="text-primary font-semibold">Wisnu</span> - an IT Guy
         </p>
         <p className="text-lg text-neutral-600">
           A passionate programmer based in Indonesia. With a love for all tech
           things, I specialize in mobile development and web development. My
           mission is to bring your ideas to life through creative
-          problem-solving. Let's collaborate and build something amazing
+          problem-solving. Let&apos;s collaborate and build something amazing
           together!
         </p>
         <div className="flex mt-6 justify-center lg:justify-start">
           <LinkButton href="#projects" className="mr-4 bg-white">
             My Work
           </LinkButton>
-          <LinkButton href="#contact">Let's Talk'</LinkButton>
+          <LinkButton href="#contact">Let&apos;s Talk</LinkButton>
         </div>
       </div>
       <Image
